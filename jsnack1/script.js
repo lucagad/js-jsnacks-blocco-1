ecb0e9d9a3f53c4700fc5c9b6af502f17e51d65f
lucagad <90861515+lucagad@users.noreply.github.com>
2022-04-12 15:58:07 +0200

    Added: Jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,17 @@
+
+/*
+JSnack 1
+L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+*/
+
+
+const firstNumber = prompt ("Inserisci il primo numero");
+const secondNumber = prompt ("Inserisci il secondo numero");
+
+console.log("Primo numero:",firstNumber);
+console.log("Secondo numero:",secondNumber);
+
+
+if(firstNumber > secondNumber){ 
+  alert("Il numero maggiore è: "+firstNumber);
+} else alert("Il numero maggiore è: "+secondNumber);
